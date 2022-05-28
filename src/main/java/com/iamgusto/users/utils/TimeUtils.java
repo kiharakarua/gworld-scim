23034327cc7e08f49b39501ad7dbaacbfe951fbf
gusto <kiharakarua@gmail.com>
2022-05-28 20:00:27 +0300

    - Fix time

--- src/main/java/com/iamgusto/users/utils/TimeUtils.java
+++ src/main/java/com/iamgusto/users/utils/TimeUtils.java
@@ -1,12 +1,11 @@
 package com.iamgusto.users.utils;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 
 public class TimeUtils {
 
   public static LocalDateTime now() {
-    return LocalDateTime.now(ZoneId.of(ZoneOffset.UTC.getId()));
+    return LocalDateTime.now(ZoneOffset.UTC);
   }
 }
